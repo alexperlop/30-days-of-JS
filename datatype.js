4b18f834a65abe2b349fd7254946a869abdd80cf
ale <ale@MacBook-Pro-de-Alejandro.local>
2022-09-29 11:35:39 +0200

    Day one done

---
+++ datatype.js
@@ -0,0 +1,8 @@
+// Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+
+import { name, betisFan, empty, proof } from './variable.js';
+
+console.log('string value:', typeof (name));
+console.log('boolean value:', typeof (betisFan));
+console.log('undefined value:', typeof (empty));
+console.log('null value:', typeof (proof));
---
+++ variable.js
@@ -0,0 +1,28 @@
+// Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
+
+const name = 'Alex';
+const betisFan = true;
+const empty = undefined;
+const proof = null;
+
+//8-. Declara variables para almacenar su nombre, apellido, estado civil, país y edad en multiples lineas;
+/*let nombre;
+let apellido;
+let estado; 
+let país;
+let edad;*/
+// 6-. Declara cuatro variables sin asignar valores
+// 9-. Declara variables para almacenar su nombre, apellido, estado civil, país y edad en una sola línea
+let nombre, apellido, estado, país, edad;
+
+nombre = 'Alex'
+apellido = 'Perez'
+estado = 'Soltero'
+país = 'España'
+edad = 28;
+
+//10-. Declara dos variables miEdad y tuEdad y asígneles valores iniciales y regístrese en la consola del navegador.
+const miEdad = 'Tengo 25 años.';
+const tuEdad = 'Tú tienes 30 años.';
+
+export { name, betisFan, empty, proof };
