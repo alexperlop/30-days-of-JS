b12a257aecd2300a9a843eb338cc17e7eeeea93f
Alex Perez <aperlop2@gmail.com>
2023-03-28 17:26:16 +0200

    day 26 finished

--- index.js
+++ index.js
@@ -1,86 +1,81 @@
 import { countriesFile } from './countries.js'
 
-const openTab = (evt, tabName) => {
-    const btnText = tabName.toLowerCase()
-    // Obtener todos los contenidos de pestaña y ocultarlos
-    const tabcontent = document.getElementsByClassName("tabcontent");
-    for (var i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+const searchByFirstLetter = (letter) => {
+    const countries = countriesFile.filter(country => country.name.toLocaleLowerCase().startsWith(letter))
+    return countries
+}
+
+const searchAnyCountry = (word) => {
+    const countries = countriesFile.filter(country => country.name.toLocaleLowerCase().includes(word))
+    return countries
+}
+
+const clearList = () => {
+    if (document.querySelector('.result-list').hasChildNodes()) {
+        document.querySelector('.result-list').innerHTML = ''
     }
+}
 
-    // Obtener todos los botones de pestaña y quitarles la clase "active"
-    const tablinks = document.getElementsByClassName("tablinks");
-    for (var i = 0; i < tablinks.length; i++) {
-        tablinks[i].classList.remove("active");
+const orderCountriesByName = (countries, boolean) => {
+    if (boolean) {
+        const orderCountries = countries.reverse()
+        clearList()
+        for (let i = 0; i < orderCountries.length; i++) {
+            const li = document.createElement('li')
+            li.textContent = orderCountries[i]
+            document.querySelector('.result-list').appendChild(li)
+        }
+        document.querySelector('.order').innerHTML = '<i class="bi bi-sort-alpha-down"></i>'
+    } else {
+        const orderCountries = countries.sort()
+        clearList()
+        for (let i = 0; i < orderCountries.length; i++) {
+            const li = document.createElement('li')
+            li.textContent = orderCountries[i]
+            document.querySelector('.result-list').appendChild(li)
+        }
+        document.querySelector('.order').innerHTML = '<i class="bi bi-sort-alpha-up-alt"></i>'
     }
-
-    // Mostrar el contenido de la pestaña seleccionada
-    document.getElementById(btnText).style.display = "flex";
-
-    // Añadir la clase "active" al botón de la pestaña seleccionada
-    evt.classList.add("active");
 }
 
-const tab = document.querySelector('.tab')
 
-tab.addEventListener('click', (e) => {
-    openTab(e.target, e.target.innerText)
+document.querySelector('.total-countries').textContent = `Total countries: ${countriesFile.length}`
+
+document.querySelector('.starting-word').addEventListener('click', () => {
+    clearList()
+    const letter = document.querySelector('.search-input').value.slice(0, 1)
+    if (letter) {
+        const countries = searchByFirstLetter(letter)
+        for (let i = 0; i < countries.length; i++) {
+            const li = document.createElement('li')
+            li.textContent = countries[i].name
+            document.querySelector('.result-list').appendChild(li)
+        }
+        document.querySelector('.text-content').style.cssText = 'display: flex'
+        document.querySelector('.countries').textContent = `Countries start with ${letter.toUpperCase()}: `
+        document.querySelector('.countries-length').textContent = countries.length
+    }
 })
 
-
-document.querySelector('.countries-length').innerText = `Currently we have ${countriesFile.length} countries`
-
-const tenMostSpokenLanguage = (countries) => {
-    const newArray = countries.reduce((acc, cur) => {
-        cur.languages.map(language => acc[language] = acc[language] ? acc[language] + 1 : 1)
-        return acc
-    }, {})
-
-    const result = Object.keys(newArray).map(key => ({
-        language: key,
-        count: newArray[key]
-    }))
-    return result.sort((a, b) => {
-        return b.count - a.count
-    }).slice(0, 10)
-}
-
-const tenMostPopulatedCountries = (countries) => {
-    return countries.map((item) => ({
-        'country': item.name,
-        'population': item.population
-    })).sort((a, b) => b.population - a.population).slice(0, 10)
-}
-
-const languages = document.querySelector('#languages ul')
-const population = document.querySelector('#population ul')
-const languagesLength = tenMostSpokenLanguage(countriesFile).length
-const populationLength = tenMostPopulatedCountries(countriesFile).length
-console.log("🚀 ~ file: index.js:70 ~ languages:", languages)
-console.log("🚀 ~ file: index.js:58 ~ population:", population)
-
-const arrayCounts = []
-tenMostSpokenLanguage(countriesFile).forEach((country) => {
-    arrayCounts.push(country.count)
+document.querySelector('.search').addEventListener('click', () => {
+    clearList()
+    const country = document.querySelector('.search-input').value
+    if (country) {
+        const countries = searchAnyCountry(country)
+        for (let i = 0; i < countries.length; i++) {
+            const li = document.createElement('li')
+            li.textContent = countries[i].name
+            document.querySelector('.result-list').appendChild(li)
+        }
+        document.querySelector('.text-content').style.cssText = 'display: flex'
+        document.querySelector('.countries').textContent = ` Countries that contains ${country}: `
+        document.querySelector('.countries-length').textContent = countries.length
+    }
 })
 
-for (let i = 0; i < languagesLength; i++) {
-    const country = tenMostSpokenLanguage(countriesFile)
-    const li = document.createElement('li')
-    languages.appendChild(li)
-    li.innerHTML = `<span>${country[i].language}</span><progress value='${country[i].count}' max='${Math.max(...arrayCounts)}'></progress ><span>${country[i].count}</span>`
-}
-
-const arrayPopulation = []
-tenMostPopulatedCountries(countriesFile).forEach((country) => {
-    arrayPopulation.push(country.population)
+let boolean = false
+document.querySelector('.order').addEventListener('click', () => {
+    const countries = [...document.querySelector('.result-list').children].map(country => country.textContent)
+    boolean = !boolean
+    if (countries.length > 0) orderCountriesByName(countries, boolean)
 })
-console.log("🚀 ~ file: index.js:74 ~ arrayPopulation:", arrayPopulation)
-
-for (let i = 0; i < populationLength; i++) {
-    const country = tenMostPopulatedCountries(countriesFile)
-    console.log("🚀 ~ file: index.js:78 ~ country:", country)
-    const li = document.createElement('li')
-    population.appendChild(li)
-    li.innerHTML = `<span>${country[i].country}</span><progress value='${country[i].population}' max='${Math.max(...arrayPopulation)}'></progress ><span>${new Intl.NumberFormat('en-US').format(country[i].population)}</span>`
-}
